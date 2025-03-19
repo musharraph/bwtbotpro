2c82614f07ab1db54d5d623eb7a08c3579516753
Musharraf Hussain <145844846+musharraph@users.noreply.github.com>
2025-03-19 20:19:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("passwordBtn").addEventListener("click", function () {
     let container = document.querySelector(".container");
     let errorMsg = document.getElementById("passwordError");
 
-    if (password === "1234") {
+    if (password === "bwt00") {
         passwordScreen.style.display = "none";
         container.style.display = "block";
     } else {
